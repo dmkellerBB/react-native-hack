28beba588d697a1aa30af3fdb79c5d4f3ed024db
Mustafa Nawabi <mustafa.nawabi@qlik.com>
2018-09-10 16:11:51 -0400

    react native elemnts for the win

--- app/screens/home/home.js
+++ app/screens/home/home.js
@@ -1,39 +1,51 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import { Button, Text } from "native-base";
+import { Card, ListItem, Button } from 'react-native-elements'
 import sharedStyle from "../../style/shared";
 
 const style = StyleSheet.create({
-	backText: {
-		color: "black"
-	}
+  backText: {
+    color: "black"
+  }
 });
 
 export default class loginScreen extends React.PureComponent {
-	static navigationOptions = props => {
-		return {
-			headerRight: (
-				<Button
-					transparent
-					iconLeft
-					style={sharedStyle.headerButton}
-					onPress={() => props.navigation.navigate("Webview")}
-				>
-					<Text style={style.backText}>Webview</Text>
-				</Button>
-			)
-		};
-	};
+  static navigationOptions = props => {
+    return {
+      headerRight: (
+        <Button
+          transparent
+          iconLeft
+          style={sharedStyle.headerButton}
+          onPress={() => props.navigation.navigate("Webview")}
+        >
+          <Text style={style.backText}>Webview</Text>
+        </Button>
+      )
+    };
+  };
 
-	render() {
-		return (
-			<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-				<Text style={{ fontSize: 30 }}>Kpi's and shit like that</Text>
-				<Button
-					onPress={() => this.props.navigation.goBack()}
-					title="Dismiss"
-				/>
-			</View>
-		);
-	}
+  render () {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text style={{ fontSize: 30 }}>Kpi's and shit like that</Text>
+        <Card
+          title='HELLO WORLD'
+          image={}>
+          <Text style={{ marginBottom: 10 }}>
+            The idea with React Native Elements is more about component structure than actual design.
+          </Text>
+          <Button
+            icon={{ name: 'code' }}
+            backgroundColor='#03A9F4'
+            buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
+            title='VIEW NOW' />
+        </Card>
+        <Button
+          onPress={() => this.props.navigation.goBack()}
+          title="Dismiss"
+        />
+      </View>
+    );
+  }
 }

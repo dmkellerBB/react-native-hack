4a6380620475fc815ef5784f80dce0fc8843a98e
tktaofik <tktaofik@users.noreply.github.com>
2018-09-10 16:52:20 -0400

    scrolling cards for home page

--- app/screens/home/home.js
+++ app/screens/home/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, ScrollView, StyleSheet } from "react-native";
 import { Card, ListItem } from "react-native-elements";
 import { Text, Button } from "native-base";
 
@@ -28,31 +28,29 @@ export default class loginScreen extends React.PureComponent {
 	};
 
 	render() {
+		const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 		return (
-			<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-				<Text style={{ fontSize: 30 }}>Kpi's and shit like that</Text>
-				<Card title="HELLO WORLD">
-					<Text style={{ marginBottom: 10 }}>
-						The idea with React Native Elements is more about component
-						structure than actual design.
-					</Text>
-					<Button
-						icon={{ name: "code" }}
-						backgroundColor="#03A9F4"
-						buttonStyle={{
-							borderRadius: 0,
-							marginLeft: 0,
-							marginRight: 0,
-							marginBottom: 0
-						}}
-						title="VIEW NOW"
-					/>
-				</Card>
-				<Button
-					onPress={() => this.props.navigation.goBack()}
-					title="Dismiss"
-				/>
-			</View>
+			<ScrollView style={{ flex: 1 }}>
+				{cards.map(key => (
+					<Card key={key} title="HELLO WORLD">
+						<Text style={{ marginBottom: 10 }}>
+							The idea with React Native Elements is more about component
+							structure than actual design.
+						</Text>
+						<Button
+							icon={{ name: "code" }}
+							backgroundColor="#03A9F4"
+							buttonStyle={{
+								borderRadius: 0,
+								marginLeft: 0,
+								marginRight: 0,
+								marginBottom: 0
+							}}
+							title="VIEW NOW"
+						/>
+					</Card>
+				))}
+			</ScrollView>
 		);
 	}
 }

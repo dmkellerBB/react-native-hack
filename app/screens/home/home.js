a7ba40a2d12123d4c7ddece964acaa45141ea9e7
Mustafa Nawabi <mustafa.nawabi@qlik.com>
2018-09-11 14:42:05 -0400

    axios and get collections

--- app/screens/home/home.js
+++ app/screens/home/home.js
@@ -2,55 +2,78 @@ import React from "react";
 import { View, ScrollView, StyleSheet } from "react-native";
 import { Card } from "react-native-elements";
 import { Text, Button } from "native-base";
+import axios from 'axios'
 
 import sharedStyle from "../../style/shared";
 
 const style = StyleSheet.create({
-	backText: {
-		color: "black"
-	}
+  backText: {
+    color: "black"
+  }
 });
 
 export default class loginScreen extends React.PureComponent {
-	static navigationOptions = props => {
-		return {
-			headerRight: (
-				<Button
-					transparent
-					iconLeft
-					style={sharedStyle.headerButton}
-					onPress={() => props.navigation.navigate("Webview")}
-				>
-					<Text style={style.backText}>Webview</Text>
-				</Button>
-			)
-		};
-	};
+  constructor (props) {
+    super(props)
+    this.onPress = this.onPress.bind(this)
+  }
 
-	render() {
-		const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-		return (
-			<ScrollView>
-				{cards.map(key => (
-					<Card key={key} title="HELLO WORLD">
-						<Text style={{ marginBottom: 10 }}>
-							The idea with React Native Elements is more about component
+  onPress () {
+    try {
+      axios({
+        method: 'get',
+        url: 'https://dev-integration2.us.qlik-stage.com/api/v1/collections',
+        headers: {
+          'Cookie': 'eas.sid=8SM5onyoU6N-If_5RP1VvyWjsTiY5rv1; eas.sid.sig=PS-2mvq0VYTDGJD9bE4sP44vM2E;'
+        }
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  static navigationOptions = props => {
+    return {
+      headerRight: (
+        <Button
+          transparent
+          iconLeft
+          style={sharedStyle.headerButton}
+          onPress={() => props.navigation.navigate("Webview")}
+        >
+          <Text style={style.backText}>Webview</Text>
+        </Button>
+      )
+    };
+  };
+
+  render () {
+    const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    return (
+      <ScrollView>
+        <Button transparent onPress={this.onPress}>
+          <Text>Click me!</Text>
+        </Button>
+        {cards.map(key => (
+          <Card key={key} title="HELLO WORLD">
+            <Text style={{ marginBottom: 10 }}>
+              The idea with React Native Elements is more about component
 							structure than actual design.
 						</Text>
-						<Button
-							icon={{ name: "code" }}
-							backgroundColor="#03A9F4"
-							buttonStyle={{
-								borderRadius: 0,
-								marginLeft: 0,
-								marginRight: 0,
-								marginBottom: 0
-							}}
-							title="VIEW NOW"
-						/>
-					</Card>
-				))}
-			</ScrollView>
-		);
-	}
+            <Button
+              icon={{ name: "code" }}
+              backgroundColor="#03A9F4"
+              buttonStyle={{
+                borderRadius: 0,
+                marginLeft: 0,
+                marginRight: 0,
+                marginBottom: 0
+              }}
+              title="VIEW NOW"
+            />
+          </Card>
+        ))}
+      </ScrollView>
+    );
+  }
 }
